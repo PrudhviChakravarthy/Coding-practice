2706cdf1fa4894bd62b850d21311fa1dc0f89e3c
prudhvichakravarthy <prudhviideas@gmail.com>
2023-10-25 11:06:27 +0530

    added bitwise.py

---
+++ python- Operators_and_Expressions/bitwise.py
@@ -0,0 +1,8 @@
+x = 5  # 0101
+y = 3  # 0011
+
+bitwise_and = x & y  # 0001
+bitwise_or = x | y   # 0111
+bitwise_xor = x ^ y  # 0110
+
+print(f"Bitwise AND: {bitwise_and}, Bitwise OR: {bitwise_or}, Bitwise XOR: {bitwise_xor}")
