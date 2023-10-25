5a0f8264adc1ef251662e2cf5e46594278531b2b
prudhvichakravarthy <prudhviideas@gmail.com>
2023-10-25 10:18:15 +0530

    added in.py

---
+++ python- Operators_and_Expressions/in.py
@@ -0,0 +1,5 @@
+fruits = ["apple", "banana", "cherry"]
+is_apple_in_fruits = "apple" in fruits
+is_orange_in_fruits = "orange" in fruits
+
+print(f"Is 'apple' in fruits: {is_apple_in_fruits}, Is 'orange' in fruits: {is_orange_in_fruits}")
