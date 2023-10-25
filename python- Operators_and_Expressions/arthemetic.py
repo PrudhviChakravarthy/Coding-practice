813646f5a80f195accdca412283eebc4e850db78
prudhvichakravarthy <prudhviideas@gmail.com>
2023-10-25 15:07:38 +0530

    added arthemetic.py

---
+++ python- Operators_and_Expressions/arthemetic.py
@@ -0,0 +1,9 @@
+x = 10
+y = 3
+addition = x + y
+subtraction = x - y
+multiplication = x * y
+division = x / y
+remainder = x % y
+
+print(f"Addition: {addition}, Subtraction: {subtraction}, Multiplication: {multiplication}, Division: {division}, Remainder: {remainder}")
