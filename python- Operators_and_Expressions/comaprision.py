9a70a1827a5114b8fa188cb8dfeae2243dc3f051
prudhvichakravarthy <prudhviideas@gmail.com>
2023-10-25 10:05:31 +0530

    added comaprision.py

---
+++ python- Operators_and_Expressions/comaprision.py
@@ -0,0 +1,8 @@
+x = 5
+y = 10
+greater_than = x > y
+less_than_or_equal = x <= y
+equal_to = x == y
+not_equal_to = x != y
+
+print(f"Greater Than: {greater_than}, Less Than or Equal: {less_than_or_equal}, Equal To: {equal_to}, Not Equal To: {not_equal_to}")
