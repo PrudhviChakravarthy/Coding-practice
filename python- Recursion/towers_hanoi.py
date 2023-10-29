b8bcd423fc4b3a7470fa75f844693491cf875975
prudhvichakravarthy <prudhviideas@gmail.com>
2023-10-29 15:54:57 +0530

    added towers_hanoi.py

---
+++ python- Recursion/towers_hanoi.py
@@ -0,0 +1,9 @@
+def towers_of_hanoi(n, source, auxiliary, target):
+    if n == 1:
+        print(f"Move disk 1 from {source} to {target}")
+        return
+    towers_of_hanoi(n - 1, source, target, auxiliary)
+    print(f"Move disk {n} from {source} to {target}")
+    towers_of_hanoi(n - 1, auxiliary, source, target)
+
+towers_of_hanoi(3, "A", "B", "C")
