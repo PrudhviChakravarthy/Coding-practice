8a9924918b1fd91850807f06f917df6ffa1c1522
Prudhvi Chakravarthy <prudhviideas@gmail.com>
2023-11-14 22:52:21 +0530

    Update update_users.js

--- node- Database_Connectivity/update_users.js
+++ node- Database_Connectivity/update_users.js
@@ -1,6 +1,6 @@
 const connection = require('./mysql_connect');
 
-const updatedData = { name: 'Updated Name', age: 35 };
+const updatedData = { name: 'deer', age: 35 };
 const recordId = 1; // Replace with the ID of the record you want to update
 
 connection.query('UPDATE users SET ? WHERE id = ?', [updatedData, recordId], (err, result) => {
